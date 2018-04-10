897ca346ee2ac7bdf407eb45043a717cf4531552
Dawid van der Hoven <dawidvdh@gmail.com>
2018-04-10 08:30:59 +0200

    remove unused

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+// @flow
+
 import React from 'react';
-import { renderToStaticMarkup, renderToString } from 'react-dom/server';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 const Html = () => (
   <html lang="en">
