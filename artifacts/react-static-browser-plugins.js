d985d669f2bcf2dd244dea9720a8af45af30e20e
dawidvdh <dawidvdh@gmail.com>
2020-02-06 15:08:21 +0100

    init react static with emotion

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,37 @@
+// Imports
+import plugin0 from '/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/node_modules/react-static-plugin-emotion",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/node_modules/react-static-plugin-typescript",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/dawidvanderhoven/code/personal/dawidvdh.github.io",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/pages/404.tsx': require('/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/pages/404.tsx').default,
+'/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/pages/about.tsx': require('/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/pages/about.tsx').default,
+'/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/pages/blog.tsx': require('/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/pages/blog.tsx').default,
+'/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/pages/index.tsx': require('/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/pages/index.tsx').default,
+'/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/containers/Post': require('/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/Users/dawidvanderhoven/code/personal/dawidvdh.github.io/src/pages/404.tsx'
+
